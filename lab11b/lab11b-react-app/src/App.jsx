77b02f9dc68e017e339dfdb132fc227fd01a5753
Muhammad Tararr <muhammadtarar194@gmail.com>
2024-03-13 23:28:38 -0600

    for tomorrow

---
+++ lab11b/lab11b-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import HeaderApp from "./components/HeaderApp.jsx";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main>
+      <HeaderApp />
+    </main>
+  );
+}
+
+export default App;
