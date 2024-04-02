098fddcf911a3e797041cc3d5ccac3698d3c4499
Muhammad Tararr <muhammadtarar194@gmail.com>
2024-04-02 14:29:08 -0600

    sure

--- lab11b/lab11b-react-app/src/App.jsx
+++ lab11b/lab11b-react-app/src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import HeaderApp from "./components/HeaderApp.jsx";
+import Company from './components/Company.jsx';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  
+  const comps =     
+    [
+      {name: "FaceBook", symbol: "FB", 
+        sector: "Internet Software", 
+        hq: "Menlo Park, California"},
+      {name: "Alphabet Inc Class A", symbol: "GOOG", 
+        sector: "Information Technology", 
+        hq: "Mountain View, California"},
+      {name: "Apple", symbol: "AAPL", 
+        sector: "Information Technology", 
+        hq: "Cupertino, California"},
+      {name: "AT&T", symbol: "T", 
+        sector: "Telecommunications Services", 
+        hq: "Dallas, Texas"}
+    ];
 
   return (
-    <main>
-      <HeaderApp />
-    </main>
+    <section  className="content box">
+    { comps.map( (c,indx) => <Company data={c} key={indx}/>)  }
+    </section>
   );
+
 }
 
-export default App;
+export default App
--- lab11b/lab11b-react-app/src/components/CompanyLogo.jsx
+++ lab11b/lab11b-react-app/src/components/CompanyLogo.jsx
@@ -1,11 +1,11 @@
 const CompanyLogo = (props) => {
-   return (
-      <div className="media-left">
-         <figure className="image is-128x128">
-         <img src={"/" + props.symbol + ".svg"} />
-         </figure>
-      </div>      
-   )
+  return (
+     <div className="media-left">
+        <figure className="image is-128x128">
+        <img src={"images/" + props.symbol + ".svg"} />
+        </figure>
+     </div>      
+  )
 }
 
 export default CompanyLogo;
